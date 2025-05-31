8f1eb8b3fdd71ea50fab07d9d01eef25d7cce224
DeNiSO22 <denis.gustavodeoliveira1976@gmail.com>
2025-05-31 16:09:54 -0300

    Delete Sidebar directory

--- Sidebar/script.js
+++
@@ -1,8 +0,0 @@
-
-
-document.querySelectorAll('.menu-item').forEach(item => {
-    item.addEventListener('click', () => {
-        const submenu = item.querySelector('.submenu');
-        submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
-        });
-});
